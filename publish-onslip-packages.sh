8629c6a63187ef2193410f86bfa7ba1ad2040d4e
Martin Blom <martin.blom@onslip.com>
2020-06-15 11:02:30 +0200

    More publishing fixes.

--- publish-onslip-packages.sh
+++ publish-onslip-packages.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-git clean -d -i
+git clean -d -x -i
 pod repo update
 bash scripts/prerelease.sh
 (cd android && npm publish)
-(cd cli && npm publish)
+(cd cli && npm install && npm publish)
