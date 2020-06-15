fdf679c603d62d1880efa639e78ffc6cbfaee1eb
Martin Blom <martin.blom@onslip.com>
2020-06-15 07:38:42 +0200

    Publishing fixes.

--- publish-onslip-packages.sh
+++ publish-onslip-packages.sh
@@ -2,12 +2,8 @@
 
 set -ex
 
-export BINTRAY_USER=user
-export BINTRAY_KEY=key
-
 git clean -d -i
 pod repo update
 bash scripts/prerelease.sh
-bash scripts/generate.sh
 (cd android && npm publish)
 (cd cli && npm publish)
